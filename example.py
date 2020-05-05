14810bbeee57a5347f3831febf2e432d0b779fa9
Markus Haack <markus.haack@gmail.com>
2020-05-05 23:19:57 +0200

    Refactor

--- example.py
+++ example.py
@@ -8,8 +8,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 CLIENT_ID = "1"
 CLIENT_SECRET = r"nCH3A0WvMYn66vGorjSrnGZ2YtjQWDiCvjg7jNxK"
-USERNAME = "mail@text.com"
-PASSWORD = "password"
+USERNAME = "markus.haack@gmail.com"
+PASSWORD = "natnyb-Vyjjiz-2piqqy"
 
 API = pylandroidcloud.LandroidAPI(CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD)
 _LOGGER.debug(f"Got auth {API}")
@@ -21,5 +21,8 @@ M = pylandroidcloud.LandroidMower(API, "30173702170204017460", 60)
 _LOGGER.debug(f"Got mower {M.mower.name}")
 
 for mower in MOWERS.mowers:
-    mower_state = pylandroidcloud.LandroidMowerState(API, mower.serial_number,)
+    mower_state = pylandroidcloud.LandroidMowerState(API, mower.serial_number)
     _LOGGER.debug(f"Got mower state {mower_state}")
+    if mower_state.state is not None:
+        _LOGGER.debug(f"Got mower state code {mower_state.state.status_code}")
+        _LOGGER.debug(f"Got mower battery {mower_state.state.battery.level}")
--- pylandroidcloud/models.py
+++ pylandroidcloud/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Any, Dict, List, cast
+from typing import Any, Dict, List, Optional, cast
 
 from pylandroidcloud.states import MowerCode, MowerErrorCode
 
@@ -43,8 +43,8 @@ class MowerStatus:
     def __init__(self, cfg: Dict[str, Any], dat: Dict[str, Any], **kwargs: Any):
         self.status_code = MowerCode(cast(int, dat.get("ls")))
         self.error_code = MowerErrorCode(cast(int, dat.get("le")))
-        self.config = Config(str(dat.get("mac")), str(dat.get("fw")), **cfg)
-        self.battery = Battery(**dat.get("bt"))  # type: ignore
+        self.config = Config(str(dat.get("fw")), **cfg)
+        self.battery = Battery(dat.get("bt"))
         self.statistics = Statistics(**dat.get("st"))  # type: ignore
         self.orientation = Orientation(cast(List[float], dat.get("dmp")))
         self.wifi_rsi = dat.get("rsi")
@@ -54,24 +54,24 @@ class MowerStatus:
 
 
 class Config:
-    __slots__ = "rain_delay", "serial_number", "mac_address", "firmware"
+    __slots__ = "rain_delay", "serial_number", "firmware"
 
-    def __init__(self, mac: str, fw: str, rd: int, sn: str, **kwargs: Any):
+    def __init__(self, fw: str, rd: int, sn: str, **kwargs: Any):
         self.rain_delay = rd
         self.serial_number = sn
-        self.mac_address = mac
         self.firmware = fw
 
 
 class Battery:
     __slots__ = "temperature", "voltage", "level", "carching", "charge_cycle"
 
-    def __init__(self, t: float, v: float, p: int, nr: int, c: int, **kwargs: Any):
-        self.temperature = t
-        self.voltage = v
-        self.level = p
-        self.carching = c
-        self.charge_cycle = nr
+    def __init__(self, battery_data: Optional[Any]):
+        if battery_data is not None:
+            self.temperature = battery_data.get("t")
+            self.voltage = battery_data.get("v")
+            self.level = battery_data.get("p")
+            self.carching = battery_data.get("c")
+            self.charge_cycle = battery_data.get("nr")
 
 
 class Statistics:
